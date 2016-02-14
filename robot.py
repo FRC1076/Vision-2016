262a0148583ddc7ba2cf6170f1118a8d963d43cb
John Crawford <ifm.cie@gmail.com>
2016-02-13 19:02:55 -0500

    Add checks for U shape
    Create a function that tests if the middle of a shape is part of the shape
    (is it a U?)
    Create a function that tests the area of a shape
    Combine those two functions to determine whether or not a shape is a U
    
    Also change all the tabs to spaces.

--- robot.py
+++ robot.py
@@ -1,84 +1,138 @@
+from __future__ import division
+
 import cv2
 import numpy as np
 
 # different image files to load for testing
-images = [""]*20
-images[0] = 'Software/Vision/Images/2016 Test.jpg'
-images[1] = 'Software/Vision/Images/2016-01-23 15-50-41.261.jpg'
-images[2] = 'Software/Vision/Images/2016-01-23 15-51-32.997.jpg'
-images[3] = 'Software/Vision/Images/2016-01-23 16-00-17.509.jpg'
-images[4] = 'Software/Vision/Images/2016-01-23 16-00-41.869.jpg'
-images[5] = 'Software/Vision/Images/2016-01-23 16-03-04.461.jpg'
-images[6] = 'Software/Vision/Images/2016-01-23 16-03-30.212.jpg'
-images[7] = 'Software/Vision/Images/2016-01-23 16-04-55.181.jpg'
-images[8] = 'Software/Vision/Images/2016-01-23 16-05-11.179.jpg'
-images[9] = 'Software/Vision/Images/2016-01-23 16-05-26.825.jpg'
-images[10] = 'Software/Vision/Images/2016-01-23 16-06-42.285.jpg'
-images[11] = 'Software/Vision/Images/2016-01-23 16-07-07.969.jpg'
-images[12] = 'Software/Vision/Images/2016-01-23 16-07-23.035.jpg'
-images[13] = 'Software/Vision/Images/2016-01-23 16-09-25.260.jpg'
-images[14] = 'Software/Vision/Images/2016-01-23 16-09-52.884.jpg'
-images[15] = 'Software/Vision/Images/2016-01-23 16-10-06.156.jpg'
-images[16] = 'Software/Vision/Images/2016-01-23 16-11-41.644.jpg'
-images[17] = 'Software/Vision/Images/2016-01-23 16-11-59.575.jpg'
-images[18] = 'Software/Vision/Images/2016-01-23 16-12-11.103.jpg'
-images[19] = 'Software/Vision/Images/GreenTape1.png'
+images = [
+    '2016 Test.jpg',
+    '2016-01-23 15-50-41.261.jpg',
+    '2016-01-23 15-51-32.997.jpg',
+    '2016-01-23 16-00-17.509.jpg',
+    '2016-01-23 16-00-41.869.jpg',
+    '2016-01-23 16-03-04.461.jpg',
+    '2016-01-23 16-03-30.212.jpg',
+    '2016-01-23 16-04-55.181.jpg',
+    '2016-01-23 16-05-11.179.jpg',
+    '2016-01-23 16-05-26.825.jpg',
+    '2016-01-23 16-06-42.285.jpg',
+    '2016-01-23 16-07-07.969.jpg',
+    '2016-01-23 16-07-23.035.jpg',
+    '2016-01-23 16-09-25.260.jpg',
+    '2016-01-23 16-09-52.884.jpg',
+    '2016-01-23 16-10-06.156.jpg',
+    '2016-01-23 16-11-41.644.jpg',
+    '2016-01-23 16-11-59.575.jpg',
+    '2016-01-23 16-12-11.103.jpg',
+    'GreenTape1.png',
+]
+images = ['../Images/' + img for img in images]
 
-img = cv2.imread(images[6],cv2.IMREAD_COLOR) #loads the image file
+img = cv2.imread(images[6],cv2.IMREAD_COLOR) # loads the image file
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #converts images from BGR to hsv
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # converts images from BGR to hsv
 
-#range of HSV color values
+# range of HSV color values
 lower_green = np.array([75,70,70])
 upper_green = np.array([95,255,255])
 
-mask = cv2.inRange(hsv, lower_green, upper_green) #creates a grayscale image using the above range of values
+mask = cv2.inRange(hsv, lower_green, upper_green) # creates a grayscale image using the above range of values
 
-res = cv2.bitwise_and(img, img, mask = mask) #uses mask to create a color image within the range values
+res = cv2.bitwise_and(img, img, mask = mask) # uses mask to create a color image within the range values
 
-edges = cv2.Canny(mask,100,200) #creates a grayscale image of just the edges of shape
+edges = cv2.Canny(mask,100,200) # creates a grayscale image of just the edges of shape
 
-#finds contours
+# finds contours
 imgray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,0)
 contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-#finds the squared distance to point
+def distance_between_points(p1, p2):
+    x1, y1 = p1[0]
+    x2, y2 = p2[0]
+    return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+
+# finds the squared distance to point
 def sq_distance_to_point(x0, y0):
-	def distance(pt):
-		x, y = pt[0]
-		dx, dy = x - x0, y - y0
-		return dx*dx + dy*dy
-	return distance
+    def distance(pt):
+        x, y = pt[0]
+        dx, dy = x - x0, y - y0
+        return dx*dx + dy*dy
+    return distance
 
-#finds the midpoint of 2 points
+# finds the midpoint of 2 points
 def midpoint(p1, p2):
-	x1, y1 = p1[0]
-	x2, y2 = p2[0]
-	return (x1 + x2)/2, (y1 + y2)/2
+    x1, y1 = p1[0]
+    x2, y2 = p2[0]
+    return (x1 + x2)/2, (y1 + y2)/2
 
-cnt = contours[0] #assigns a specific contour to be the U (should be replaced by contour test)
+def area(cnt):
+    return cv2.contourArea(cnt)
 
-#finds the extreme corners of the contour
+def perimeter(cnt):
+    return cv2.actLength(cnt,True)
+
+def five_eighths_test(cnt, img):
+    # finds the diagonal extreme of the contour
+    upper_left = min(cnt, key=sq_distance_to_point(0, 0))
+    upper_right = min(cnt, key=sq_distance_to_point(640, 0))
+    bottom_left = min(cnt, key=sq_distance_to_point(0, 480))
+    bottom_right = min(cnt, key=sq_distance_to_point(640, 480))
+
+    # finds height of the U
+    left_height = distance_between_points(upper_left, bottom_left)
+    right_height = distance_between_points(upper_right, bottom_right)
+    height = (left_height + right_height)/2
+
+    # finds the midpoint
+    upper_mid = midpoint(upper_left, upper_right)
+    # unpacks the x, y values of the midpoint
+    midX, midY = upper_mid
+
+    testX = midX
+    testY = midY + (5/8)*height
+    testpoint = testX, testY
+    return (img[midY:testY, testX] == 0).all()
+
+def area_test(cnt):
+    return 400 < area(cnt) < 2500
+
+print len(contours)
+for contour in contours:
+    text = ""
+    if five_eighths_test(contour, mask) and area_test(contour):
+        text = "True"
+    else:
+        text = "False"
+    label_point = min(contour, key=sq_distance_to_point(640, 480))
+    labelX, labelY = label_point[0]
+    font = cv2.FONT_HERSHEY_PLAIN
+    cv2.putText(edges,text,(labelX,labelY), font, 1.0, (255,255,255), 1, False)
+
+cnt = contours[1] # assigns a specific contour to be the U (should be replaced by contour test)
+
+print five_eighths_test(cnt, mask)
+print area_test(cnt)
+
+# finds the diagonal extreme of the contour
 upper_left = min(cnt, key=sq_distance_to_point(0, 0))
 upper_right = min(cnt, key=sq_distance_to_point(640, 0))
 bottom_left = min(cnt, key=sq_distance_to_point(0, 480))
 bottom_right = min(cnt, key=sq_distance_to_point(640, 480))
 
-midpoint = midpoint(upper_left, upper_right) #finds the midpoint
-midX, midY = midpoint #unpacks the x, y values of the midpoint
+midpoint = midpoint(upper_left, upper_right) # finds the midpoint
+midX, midY = midpoint # unpacks the x, y values of the midpoint
 
-M = cv2.moments(cnt) #finds moments of the contour
-area = cv2.contourArea(cnt) #finds the area of the contour
-perimeter = cv2.arcLength(cnt,True) #finds the perimeter of the contour
-ratio = perimeter*perimeter / area #finds the ratio of perimeter squared to area
+M = cv2.moments(cnt) # finds moments of the contour
+area = cv2.contourArea(cnt) # finds the area of the contour
+perimeter = cv2.arcLength(cnt,True) # finds the perimeter of the contour
 
-#outputs different images
+# outputs different images
 cv2.imshow('image',img)
 cv2.imshow('mask', mask)
 cv2.imshow('edges', edges)
 
-#waits for the ESc key to exit
+# waits for the ESc key to exit
 k = cv2.waitKey(0)
 if k == 27:
     cv2.destroyAllWindows()
