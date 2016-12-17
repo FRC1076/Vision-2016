769b6c02b2cb3075aace195332aebaf64dcbad1e
Mark Menge <markmenge@gmail.com>
2016-12-17 19:49:38 +0000

    A shell script for setting the driver level settings for the robot's camera.

---
+++ set_camera_params.sh
@@ -0,0 +1,3 @@
+# These are the commands to confgure the camera.
+
+v4l2-ctl --device=/dev/video0 -c gain_automatic=0 -c while_balance_automatic=0 -c exposure=128 -c gain=0 -c auto_exposure=0 -c brightness=0 -c hue=-32 -c saturation=96
