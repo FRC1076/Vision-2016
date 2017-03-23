2e0c34c1cba33c5f0fa940affe4a23b48bf095ed
Matthew Jones <mcolinj@mac.com>
2017-03-23 02:45:14 +0000

    Reduce exposure

--- set_camera_params.sh
+++ set_camera_params.sh
@@ -1,3 +1,3 @@
 # These are the commands to confgure the camera.
 
-v4l2-ctl --device=/dev/video0 -c gain_automatic=0 -c white_balance_automatic=0 -c exposure=50 -c gain=0 -c auto_exposure=1 -c brightness=0 -c hue=-32 -c saturation=96
+v4l2-ctl --device=/dev/video0 -c gain_automatic=0 -c white_balance_automatic=0 -c exposure=35 -c gain=0 -c auto_exposure=1 -c brightness=0 -c hue=-32 -c saturation=96
